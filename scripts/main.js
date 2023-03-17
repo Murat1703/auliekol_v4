a036b58cb487cb2a1c0c37064f987e9ce634b1a1
Murat Kuzhakhmetov <105972017+Murat1703@users.noreply.github.com>
2023-03-17 12:01:31 +0600

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+  $('.galery-content').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: $('.nav-l'),
+    nextArrow: $('.nav-r'),
+});
+
+
+$('.partners-slider').slick({
+  infinite: true,
+  dots: true,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+});
